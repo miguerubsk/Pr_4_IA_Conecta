aca5e0a72fe75bab58b03196db266e597fbd7f26
Miguel González García <mgg00112@red.ujaen.es>
2021-04-26 14:10:24 +0200

    Update Conecta.java

--- src/conecta/Conecta.java
+++ src/conecta/Conecta.java
@@ -42,9 +42,9 @@ public class Conecta extends JFrame implements ActionListener {
 
     // Parámetros
     // Número de filas
-    private static final int filas = 4;
+    private static final int filas = 6;
     // Número de columnas
-    private static final int columnas = 4;
+    private static final int columnas = 7;
     // Número de fichas que han de alinearse para ganar
     private static final int conecta = 4;
 
