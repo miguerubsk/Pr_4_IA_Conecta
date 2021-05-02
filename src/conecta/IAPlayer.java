c1697b90bf99b4a68136a1da2e3476a8edffa34a
Miguel González García <mgg00112@red.ujaen.es>
2021-05-02 13:50:02 +0200

    Update IAPlayer.java

--- src/conecta/IAPlayer.java
+++ src/conecta/IAPlayer.java
@@ -19,7 +19,6 @@ package conecta;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
-import javafx.util.Pair;
 
 /**
  *
@@ -695,4 +694,28 @@ public class IAPlayer extends Player {
 
     }
 
+    public class Pair<K extends Object, V extends Object> {
+
+        private K key;
+        private V value;
+
+        public Pair(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        public K getKey() {
+            return key;
+        }
+
+        public V getValue() {
+            return value;
+        }
+
+        @Override
+        public String toString() {
+            return "Pair{" + "key=" + key + ", value=" + value + '}';
+        }
+
+    }
 }
